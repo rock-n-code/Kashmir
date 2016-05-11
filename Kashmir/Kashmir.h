5269fe053763e2927ea8c0d234aeb676090f72b7
Javier Cicchelli <javier@rock-n-co.de>
2016-05-11 16:38:02 +0200

    Initial Commit

---
+++ Kashmir/Kashmir.h
@@ -0,0 +1,19 @@
+//
+//  Kashmir.h
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 11/05/16.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Kashmir.
+FOUNDATION_EXPORT double KashmirVersionNumber;
+
+//! Project version string for Kashmir.
+FOUNDATION_EXPORT const unsigned char KashmirVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kashmir/PublicHeader.h>
+
+
