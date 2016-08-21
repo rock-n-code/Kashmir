9cb519d341c1507c64f683f16864c189ee4f3d2e
Javier Cicchelli <javier@rock-n-co.de>
2016-08-21 11:47:43 +0200

    Created NSObjectExtensions.

---
+++ Kashmir/Shared/NSObjectExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  NSObjectExtensions.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 21/08/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+
+}
---
+++ KashmirTests/NSObjectExtensionsTests.swift
@@ -0,0 +1,25 @@
+//
+//  NSObjectExtensionsTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 21/08/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class NSObjectExtensionsTests: XCTestCase {
+
+	// MARK: - Setup
+
+	override func setUp() {
+		super.setUp()
+	}
+
+	override func tearDown() {
+		super.tearDown()
+	}
+
+}
