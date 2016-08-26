1fe24d77cb845aa9173374915ad65b72010e8064
Javier Cicchelli <javier@rock-n-co.de>
2016-08-26 16:27:15 +0200

    Created BundleExtensions.

---
+++ Kashmir/Shared/BundleExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  BundleExtensions.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 26/08/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+
+	// ...
+
+}
