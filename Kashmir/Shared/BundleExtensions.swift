af4f46bc52f3d6d05a888605a2a0dd12bb02237b
Javier Cicchelli <javier@rock-n-co.de>
2016-08-27 06:05:16 +0200

    Implemented the "url(forResource: withExtension: )" function on BundleExtensions.

--- Kashmir/Shared/BundleExtensions.swift
+++ Kashmir/Shared/BundleExtensions.swift
@@ -10,6 +10,31 @@ import Foundation
 
 extension Bundle {
 
-	// ...
+	// MARK: - Functions
+
+	static func url(forResource named: String, withExtension fileExtension: String? = nil) -> URL? {
+		var url: URL?
+
+		for framework in allFrameworks {
+			url = framework.url(forResource: named, withExtension: fileExtension)
+
+			guard url == nil
+			else {
+				return url
+			}
+		}
+
+		for bundle in allBundles {
+			url = bundle.url(forResource: named, withExtension: fileExtension)
+
+			guard url == nil
+			else {
+				break
+			}
+		}
+
+		return url
+	}
+
 
 }
