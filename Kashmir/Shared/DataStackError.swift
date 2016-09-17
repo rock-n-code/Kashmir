c40a0e5f835b63bc290c54f3e5624fd53cb27c4e
Javier Cicchelli <javier@rock-n-co.de>
2016-09-17 07:01:55 +0200

    Improved the existing documentation a bit.

--- Kashmir/Shared/DataStackError.swift
+++ Kashmir/Shared/DataStackError.swift
@@ -10,14 +10,14 @@ import Foundation
 
 /// Representation of the errors that could be encountered while using *DataStack*.
 ///
-/// - **objectModelNotFound**: a file of extension *.momd* containing the object model to use with *CoreData* was not found on any of the bundles included on the project or workspace.
-/// - **objectModelNotCreated**: the *CoreData* managed object model have not been created on the current bundle by its initializer.
-/// - **storeUrlNotFound**: a persistance store containing the physical database have not been found on the file system.
-/// - **containerNotFound**: a *NSPersistentContainer* instance have not been found on the list of containers managed by *DataStack*.
-/// - **containerNameIsEmpty**: a container name passed to *DataStack* is actually an empty string.
-/// - **containerNameExists**: a container name passed to *DataStack* does exist on the list of containers it manages.
-/// - **containerNameNotExists**: a container name passed to *DataStack* does not exist on the list of containers it manages.
-/// - **other**: any other type of *NSError* encountered while using *DataStack*.
+/// - **objectModelNotFound**:		A file of extension *.momd* containing the object model to use with *CoreData* was not found on any of the bundles included on the project or workspace.
+/// - **objectModelNotCreated**:	A *CoreData* managed object model have not been created on the current bundle by its initializer.
+/// - **storeUrlNotFound**:			A persistance store containing the physical database have not been found on the file system.
+/// - **containerNotFound**:		A *NSPersistentContainer* instance have not been found on the list of containers managed by *DataStack*.
+/// - **containerNameIsEmpty**:		A container name passed to *DataStack* is actually an empty string.
+/// - **containerNameExists**:		A container name passed to *DataStack* does exist on the list of containers it manages.
+/// - **containerNameNotExists**:	A container name passed to *DataStack* does not exist on the list of containers it manages.
+/// - **other**:					Any other type of *NSError* encountered while using *DataStack*.
 public enum DataStackError: Error {
 	case objectModelNotFound
 	case objectModelNotCreated
