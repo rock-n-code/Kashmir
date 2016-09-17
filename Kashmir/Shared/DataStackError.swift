6762833a3ff36dbb17f3a4d80881b6cf4545c06e
Javier Cicchelli <javier@rock-n-co.de>
2016-09-17 06:34:12 +0200

    Documented the errors types on DataStackError.

--- Kashmir/Shared/DataStackError.swift
+++ Kashmir/Shared/DataStackError.swift
@@ -8,13 +8,23 @@
 
 import Foundation
 
+/// Representation of the errors that could be encountered while using *DataStack*.
+///
+/// - **objectModelNotFound**: a file of extension *.momd* containing the object model to use with *CoreData* was not found on any of the bundles included on the project or workspace.
+/// - **objectModelNotCreated**: the *CoreData* managed object model have not been created on the current bundle by its initializer.
+/// - **storeUrlNotFound**: a persistance store containing the physical database have not been found on the file system.
+/// - **containerNotFound**: a *NSPersistentContainer* instance have not been found on the list of containers managed by *DataStack*.
+/// - **containerNameIsEmpty**: a container name passed to *DataStack* is actually an empty string.
+/// - **containerNameExists**: a container name passed to *DataStack* does exist on the list of containers it manages.
+/// - **containerNameNotExists**: a container name passed to *DataStack* does not exist on the list of containers it manages.
+/// - **other**: any other type of *NSError* encountered while using *DataStack*.
 public enum DataStackError: Error {
-	case ObjectModelNotFound
-	case ObjectModelNotCreated
-	case ContainerNotFound
-	case StoreUrlNotFound
-	case ContainerNameIsEmpty
-	case ContainerNameExists
-	case ContainerNameNotExists
-	case Other(NSError)
+	case objectModelNotFound
+	case objectModelNotCreated
+	case storeUrlNotFound
+	case containerNotFound
+	case containerNameIsEmpty
+	case containerNameExists
+	case containerNameNotExists
+	case other(NSError)
 }
