6c0b47c957c82723ab5be99e939213cc4562bb65
Javier Cicchelli <javier@rock-n-code.com>
2016-11-14 23:24:00 +0100

    Created and setup the Kashmir_iOS and Kashmir_iOSTests targets.

---
+++ Kashmir/Kashmir_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Kashmir_iOS.h
+//  Kashmir_iOS
+//
+//  Created by Javier Cicchelli on 14/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kashmir_iOS.
+FOUNDATION_EXPORT double Kashmir_iOSVersionNumber;
+
+//! Project version string for Kashmir_iOS.
+FOUNDATION_EXPORT const unsigned char Kashmir_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kashmir_iOS/PublicHeader.h>
+
+
