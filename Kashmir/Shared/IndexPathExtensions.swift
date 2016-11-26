036a3217b2597a843d8adc54cbb02f9e7a0331f7
Javier Cicchelli <javier@rock-n-code.com>
2016-11-26 23:02:39 +0100

    Created IndexPathExtensions and implemented its "previousSection", "previousItem", "nextSection" and "nextItem" properties.

---
+++ Kashmir/Shared/IndexPathExtensions.swift
@@ -0,0 +1,51 @@
+//
+//  IndexPathExtensions.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 26/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+extension IndexPath {
+    
+    // MARK: Properties
+    
+    /// Creates a new index path positioned at a previous section if possible.
+    ///
+    /// - note: The new `IndexPath` instance is generated out of the same item number but a decreased section number from the current index path.
+    var previousSection: IndexPath? {
+        guard section > 0  else {
+            return nil
+        }
+        
+        return IndexPath(item: item, section: section - 1)
+    }
+    
+    /// Creates a new index path positioned at a previous item if possible.
+    ///
+    /// - note: The new `IndexPath` instance is generated out of the same section number but a decreased item number from the current index path.
+    var previousItem: IndexPath? {
+        guard item > 0 else {
+            return nil
+        }
+
+        return IndexPath(item: item - 1, section: section)
+    }
+    
+    /// Creates a new index path positioned at a next section.
+    ///
+    /// - note: The new `IndexPath` instance is generated out of the same item number but an increased section number from the current index path.
+    var nextSection: IndexPath {
+        return IndexPath(item: item, section: section + 1)
+    }
+    
+    /// Creates a new index path positioned at a next item.
+    ///
+    /// - note: The new `IndexPath` instance is generated out of the same section number but an increased item number from the current index path.
+    var nextItem: IndexPath {
+        return IndexPath(item: item + 1, section: section)
+    }
+    
+}
