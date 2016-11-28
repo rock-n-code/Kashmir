16d899140c0ceeca82e888c1131aa695dbc687d5
Javier Cicchelli <javier@rock-n-code.com>
2016-11-28 18:37:36 +0100

    Created Network.

---
+++ Kashmir/Shared/Network.swift
@@ -0,0 +1,10 @@
+//
+//  Network.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 28/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+/// The structure that contains the network data and enumeration types.
+public struct Network {}
