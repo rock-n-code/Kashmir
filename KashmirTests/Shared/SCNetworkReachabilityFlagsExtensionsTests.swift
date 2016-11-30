8b5ca698f77448ca6643a33bfeaa93b05244ad79
Javier Cicchelli <javier@rock-n-code.com>
2016-11-30 01:10:02 +0100

    Created and implemented several properties on SCNetworkReachabilityFlagsExtensions.

---
+++ KashmirTests/Shared/SCNetworkReachabilityFlagsExtensionsTests.swift
@@ -0,0 +1,200 @@
+//
+//  SCNetworkReachabilityFlagsExtensionsTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 29/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import XCTest
+import SystemConfiguration
+
+@testable import Kashmir
+
+class SCNetworkReachabilityFlagsExtensionsTests: XCTestCase {
+    
+    // MARK: Setup
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: Properties tests
+
+    func testIsReachable() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1)
+
+        XCTAssertTrue(flags.isReachable)
+    }
+    
+    func testIsNotReachable() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0)
+
+        XCTAssertFalse(flags.isReachable)
+    }
+    
+    func testIsConnectionRequired() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 2)
+        
+        XCTAssertTrue(flags.isConnectionRequired)
+    }
+    
+    func testIsConnectionNotRequired() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0 + 1 << 1)
+        
+        XCTAssertFalse(flags.isConnectionRequired)
+    }
+    
+    func testIsInterventionRequired() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 4)
+        
+        XCTAssertTrue(flags.isInterventionRequired)
+    }
+    
+    func testIsInterventionNotRequired() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 2 + 1 << 3)
+        
+        XCTAssertFalse(flags.isInterventionRequired)
+    }
+
+    func testIsConnectionOnTraffic() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 3)
+        
+        XCTAssertTrue(flags.isConnectionOnTraffic)
+    }
+    
+    func testIsConnectionNotOnTraffic() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1 + 1 << 2)
+        
+        XCTAssertFalse(flags.isConnectionOnTraffic)
+    }
+    
+    func testIsConnectionOnDemand() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 5)
+        
+        XCTAssertTrue(flags.isConnectionOnDemand)
+    }
+    
+    func testIsConnectionNotOnDemand() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 3 + 1 << 4)
+        
+        XCTAssertFalse(flags.isConnectionOnDemand)
+    }
+    
+    func testIsTransientConnection() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0)
+        
+        XCTAssertTrue(flags.isTransientConnection)
+    }
+    
+    func testIsNotTransientConnection() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1 + 1 << 2)
+        
+        XCTAssertFalse(flags.isTransientConnection)
+    }
+    
+    func testIsALocalAddress() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 16)
+        
+        XCTAssertTrue(flags.isALocalAddress)
+    }
+    
+    func testIsNotALocalAddress() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 15 + 1 << 17)
+        
+        XCTAssertFalse(flags.isALocalAddress)
+    }
+
+    func testIsDirectConnection() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 17)
+        
+        XCTAssertTrue(flags.isDirectConnection)
+    }
+    
+    func testIsNotDirectConnection() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 16 + 1 << 18)
+        
+        XCTAssertFalse(flags.isDirectConnection)
+    }
+
+    func testIsConnectionOnTrafficOrDemand() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 3 + 1 << 5)
+        
+        XCTAssertTrue(flags.isConnectionOnTrafficOrDemand)
+    }
+    
+    func testIsNotConnectionOnTrafficNorDemand() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0 + 1 << 4)
+        
+        XCTAssertFalse(flags.isConnectionOnTrafficOrDemand)
+    }
+    
+    func testIsConnectionRequiredOrTransient() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0 + 1 << 2)
+        
+        XCTAssertTrue(flags.isConnectionRequiredOrTransient)
+    }
+    
+    func testIsNotConnectionRequiredNorTransient() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1 + 1 << 5)
+        
+        XCTAssertFalse(flags.isConnectionRequiredOrTransient)
+    }
+    
+    func testIsConnected() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1)
+        
+        XCTAssertTrue(flags.isConnected)
+    }
+    
+    func testIsNotConnected() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0)
+        
+        XCTAssertFalse(flags.isConnected)
+    }
+    
+    func testIsOnWWAN() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 18)
+        
+        #if os(iOS)
+            XCTAssertTrue(flags.isOnWWAN)
+        #else
+            XCTAssertFalse(flags.isOnWWAN)
+        #endif
+    }
+    
+    func testIsNotOnWWAN() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 17)
+        
+        XCTAssertFalse(flags.isOnWWAN)
+    }
+    
+    func testIsReachableViaWWAN() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1 + 1 << 18)
+        
+        XCTAssertTrue(flags.isReachableViaWWAN)
+    }
+    
+    func testIsNotReachableViaWWAN() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0 + 1 << 18)
+        
+        XCTAssertFalse(flags.isReachableViaWWAN)
+    }
+    
+    func testIsReachableViaWiFi() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 1)
+        
+        XCTAssertTrue(flags.isReachableViaWiFi)
+    }
+    
+    func testIsNotReachableViaWiFi() {
+        let flags = SCNetworkReachabilityFlags(rawValue: 1 << 0)
+        
+        XCTAssertFalse(flags.isReachableViaWiFi)
+    }
+
+}
