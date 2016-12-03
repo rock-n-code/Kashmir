7e468c89e1512931edaf949560b9ccc2078a6065
Javier Cicchelli <javier@rock-n-code.com>
2016-12-03 08:40:38 +0100

    Documented SCNetworkReachabilityFlagsExtensions.

--- Kashmir/Shared/SCNetworkReachabilityFlagsExtensions.swift
+++ Kashmir/Shared/SCNetworkReachabilityFlagsExtensions.swift
@@ -10,50 +10,64 @@ import SystemConfiguration
 
 public extension SCNetworkReachabilityFlags {
     
+    /// Checks if the `reachable` flag is included.
     var isReachable: Bool {
         return contains(.reachable)
     }
     
+    /// Checks if the `connectionRequired` flag is included.
     var isConnectionRequired: Bool {
         return contains(.connectionRequired)
     }
     
+    /// Checks if the `interventionRequired` flag is included.
     var isInterventionRequired: Bool {
         return contains(.interventionRequired)
     }
     
+    /// Checks if the `connectionOnTraffic` flag is included.
     var isConnectionOnTraffic: Bool {
         return contains(.connectionOnTraffic)
     }
     
+    /// Checks if the `connectionOnDemand` flag is included.
     var isConnectionOnDemand: Bool {
         return contains(.connectionOnDemand)
     }
     
+    /// Checks if the `transientConnection` flag is included.
     var isTransientConnection: Bool {
         return contains(.transientConnection)
     }
     
+    /// Checks if the `isLocalAddress` flag is included.
     var isALocalAddress: Bool {
         return contains(.isLocalAddress)
     }
     
+    /// Checks if the `isDirectConnection` flag is included.
     var isDirectConnection: Bool {
         return contains(.isDirect)
     }
     
+    /// Checks if either the `connectionOnTraffic` flag or the `connectionOnDemand` flag is included.
     var isConnectionOnTrafficOrDemand: Bool {
         return isConnectionOnTraffic || isConnectionOnDemand
     }
     
+    /// Checks if either the `connectionRequired` flag or the `transientConnection` flag is included.
     var isConnectionRequiredOrTransient: Bool {
         return isConnectionRequired || isTransientConnection
     }
     
+    /// Checks if the `reachable` flag is included while the `connectionRequired` flag is not included.
     var isConnected: Bool {
         return isReachable && !isConnectionRequired
     }
     
+    /// Checks if the `isWWAN` flag in included.
+    ///
+    /// - note: The `isWWAN` flag only exists on the **iOS** platform, on other platforms the value `false` is returned.
     var isOnWWAN: Bool {
         #if os(iOS)
             return contains(.isWWAN)
@@ -62,6 +76,9 @@ public extension SCNetworkReachabilityFlags {
         #endif
     }
     
+    /// Checks if a device is able to connect to the network via a WWAN modem.
+    ///
+    /// - note: The WWAN modems are used by **iOS** devices only, on other types of devices it just checks if the device is able to connect to the network.
     var isReachableViaWWAN: Bool {
         #if os(iOS)
             return isConnected && isOnWWAN
@@ -70,6 +87,7 @@ public extension SCNetworkReachabilityFlags {
         #endif
     }
     
+    /// Checks if a device is able to connect to the network via a WiFi modem.
     var isReachableViaWiFi: Bool {
         #if os(iOS)
             return isConnected && !isOnWWAN
