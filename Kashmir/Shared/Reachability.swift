6673ee202871606f47d250177f345ca8e0d0667d
Javier Cicchelli <javier@rock-n-code.com>
2016-12-13 00:07:04 +0100

    Created Reachability.

---
+++ Kashmir/Shared/Reachability.swift
@@ -0,0 +1,15 @@
+//
+//  Reachability.swift
+//  Vinyls
+//
+//  Created by Javier Cicchelli on 27/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import SystemConfiguration
+
+public class Reachability {
+    
+    // ...
+    
+}
