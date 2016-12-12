9a0abcd8399c23372c73ca20d6e73a08d932f92d
Javier Cicchelli <javier@rock-n-code.com>
2016-12-13 00:07:04 +0100

    Created Reachability.

---
+++ KashmirTests/Shared/ReachabilityTests.swift
@@ -0,0 +1,25 @@
+//
+//  ReachabilityTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 29/11/2016.
+//  Copyright © 2016 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class ReachabilityTests: XCTestCase {
+    
+    // MARK: Setup
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
