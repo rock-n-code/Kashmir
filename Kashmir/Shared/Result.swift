bc6443d25d6eff162f943d760df83d3688d7a8a0
Javier Cicchelli <javier@rock-n-code.com>
2017-01-07 11:15:23 +0100

    Created Result.

---
+++ Kashmir/Shared/Result.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 07/01/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+public struct Result {
+	
+	// ...
+	
+}
---
+++ KashmirTests/Shared/ResultTests.swift
@@ -0,0 +1,27 @@
+//
+//  ResultTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 07/01/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class ResultTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+	
+	// MARK: Initializations tests
+
+}
