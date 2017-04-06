23629e227c008b9e2d97a952b8497c27757377f3
Javier Cicchelli <javier@rock-n-code.com>
2017-04-06 19:23:46 +0200

    Created UIImageExtensions and implemented its “mask(withColor: , isOpaque: )” function.

---
+++ KashmirTests/iOS/UIImageExtensionsTests.swift
@@ -0,0 +1,25 @@
+//
+//  UIImageExtensionsTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 06/04/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class UIImageExtensionsTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+	
+}
