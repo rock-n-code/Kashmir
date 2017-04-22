e77ec63e66b21206752f1f112ced3ffe2ee0f395
Javier Cicchelli <javier@rock-n-code.com>
2017-04-23 00:00:20 +0200

    Created and defined “iso8601FullDateTimeUTC” constants on DateFormatterFormat.

---
+++ Kashmir/Shared/DateFormatterFormat.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatterFormat.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 22/04/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+public extension DateFormatter {
+	
+	// MARK: Constants
+	
+	struct Format {
+		static var iso8601FullDateTimeUTC = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+	}
+	
+}
