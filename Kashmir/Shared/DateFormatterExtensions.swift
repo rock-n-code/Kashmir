5e30308e33208757c146d4dcc819f55d55356d31
Javier Cicchelli <javier@rock-n-code.com>
2017-04-22 17:55:50 +0200

    Created DateFormatterExtensions.

---
+++ Kashmir/Shared/DateFormatterExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  DateFormatterExtensions.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 22/04/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+public extension DateFormatter {
+	
+	// ...
+	
+}
---
+++ KashmirTests/Shared/DateFormatterExtensionsTests.swift
@@ -0,0 +1,25 @@
+//
+//  DateFormatterExtensionsTests.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 22/04/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class DateFormatterExtensionsTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+
+}
