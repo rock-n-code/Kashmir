a369b7652184c4e393a20125b0b767bf0f468eae
Javier Cicchelli <javier@rock-n-code.com>
2017-06-11 22:48:16 +0200

    Created Blocks and defined ExecutionBlock.

---
+++ Kashmir/Shared/Blocks.swift
@@ -0,0 +1,9 @@
+//
+//  Blocks.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 11/06/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+typealias ExecutionBlock = (() -> ())
