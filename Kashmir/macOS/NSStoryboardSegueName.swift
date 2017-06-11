1f51339d99d1e0eab1dd6ba8da22eca0b86f996f
Javier Cicchelli <javier@rock-n-code.com>
2017-06-11 22:54:02 +0200

    Created NSStoryboardName and NSStoryboardSegueName.

---
+++ Kashmir/macOS/NSStoryboardSegueName.swift
@@ -0,0 +1,20 @@
+//
+//  NSStoryboardSegueName.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 11/06/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import AppKit
+
+public extension NSStoryboardSegue {
+
+	// MARK: Constants
+	
+	/**
+	The structure which contains the names of the segues used with a `NSStoryboardSegue` instance defined as constants.
+	*/
+	public struct Name {}
+	
+}
