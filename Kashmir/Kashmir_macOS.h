bd78cdbf0c222b91c353f403ed587ef3b15e244b
Javier Cicchelli <javier@rock-n-code.com>
2017-06-11 21:00:46 +0200

    Created Kashmir_macOS target.

---
+++ Kashmir/Kashmir_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Kashmir_macOS.h
+//  Kashmir_macOS
+//
+//  Created by Javier Cicchelli on 11/06/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Kashmir_macOS.
+FOUNDATION_EXPORT double Kashmir_macOSVersionNumber;
+
+//! Project version string for Kashmir_macOS.
+FOUNDATION_EXPORT const unsigned char Kashmir_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kashmir_macOS/PublicHeader.h>
+
+
