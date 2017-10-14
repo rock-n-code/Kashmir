a85a64de6a95eaa1e5a092c79f67f3f7d0e96451
Javier Cicchelli <javier@rock-n-code.com>
2017-10-14 22:27:59 +0200

    Created TimeZoneExtensions.

---
+++ Kashmir/Shared/TimeZoneExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  TimeZoneExtensions.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 14/10/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import Foundation
+
+public extension TimeZone {
+	
+	// ...
+	
+}
---
+++ KashmirTests/Shared/TimeZoneExtensionsTests.swift
@@ -0,0 +1,44 @@
+//
+//  TimeZoneExtensionsTests.swift
+//  KashmirTests
+//
+//  Created by Javier Cicchelli on 14/10/2017.
+//  Copyright © 2017 Rock & Code. All rights reserved.
+//
+
+import XCTest
+import Foundation
+
+@testable import Kashmir
+
+class TimeZoneExtensionsTests: XCTestCase {
+
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+	
+	// MARK: Initializers tests
+	
+	func testInitWithUTCDateString() {
+		XCTFail("Not implemented yet")
+	}
+	
+	func testInitWithOffsetDateString() {
+		XCTFail("Not implemented yet")
+	}
+	
+	func testInitWithEmptyString() {
+		XCTFail("Not implemented yet")
+	}
+	
+	func testInitWithWrongDateString() {
+		XCTFail("Not implemented yet")
+	}
+
+}
