bed941cee1bb596a1e1aaebcd71f619f01fda1c6
Javier Cicchelli <javier@rock-n-code.com>
2017-10-20 10:29:46 +0200

    Improved the "init(dateString: )" initializer in TimeZoneExtensions by using the ~~> operator.

--- Kashmir/Shared/TimeZoneExtensions.swift
+++ Kashmir/Shared/TimeZoneExtensions.swift
@@ -13,45 +13,43 @@ public extension TimeZone {
 	// MARK: Constants
 	
 	private struct RegularExpression {
-		static let utc = "z$"
+		static let utc = "[z|Z]$"
 		static let offset = "[-|+]\\d{2}[:]\\d{2}$"
 	}
 	
 	// MARK: Initializers
 	
+	/**
+	Initialize a `TimeZone` instance with a date formatted in ISO8601 format.
+	
+	- parameter dateString: a date string formatted in ISO8601 format.
+	- note: In case the date string does not comply with the requirements of the ISO8601 format, no instance will be created.
+	*/
 	init?(dateString: String) {
-		do {
-			let wholeRange = NSRange(dateString.startIndex..., in: dateString)
-			let utcExpression = try NSRegularExpression(pattern: RegularExpression.utc, options: .caseInsensitive)
-			let offsetExpression = try NSRegularExpression(pattern: RegularExpression.offset, options: .caseInsensitive)
-			let utcMatches = utcExpression.matches(in: dateString, range: wholeRange)
-			let offsetMatches = offsetExpression.matches(in: dateString, range: wholeRange)
-			
-			guard !utcMatches.isEmpty || !offsetMatches.isEmpty else {
-				return nil
-			}
-			
-			var secondsFromGMT = 0
-			
-			if let range = offsetMatches.first?.range {
-				let substring = (dateString as NSString).substring(with: range)
-				let components = substring.components(separatedBy: ":")
-				
-				if
-					let firstComponent = components.first,
-					let lastComponent = components.last,
-					let hours = Int(firstComponent),
-					let minutes = Int(lastComponent)
-				{
-					secondsFromGMT = hours * 3600 + minutes * 60
-				}
-			}
-			
-			self.init(secondsFromGMT: secondsFromGMT)
-		}
-		catch {
+		let utc = RegularExpression.utc ~~> dateString
+		let offset = RegularExpression.offset ~~> dateString
+		
+		guard !utc.isEmpty || !offset.isEmpty else {
 			return nil
 		}
+		
+		var secondsFromGMT = 0
+		
+		if let range = offset.first?.range {
+			let substring = (dateString as NSString).substring(with: range)
+			let components = substring.components(separatedBy: ":")
+			
+			if
+				let firstComponent = components.first,
+				let lastComponent = components.last,
+				let hours = Int(firstComponent),
+				let minutes = Int(lastComponent)
+			{
+				secondsFromGMT = hours * 3600 + minutes * 60
+			}
+		}
+		
+		self.init(secondsFromGMT: secondsFromGMT)
 	}
 	
 }
