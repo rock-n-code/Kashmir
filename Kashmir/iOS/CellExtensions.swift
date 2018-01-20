fe849509eb04e8f1ae8a75202c0f80eca68cb5b8
Javier Cicchelli <javier@rock-n-code.com>
2018-01-20 18:34:41 +0100

    Extended CellExtensions to support "registerNib(in: )" and "dequeue(from: , at: )" static function for UICollectionViewCell.

--- Kashmir/iOS/CellExtensions.swift
+++ Kashmir/iOS/CellExtensions.swift
@@ -36,7 +36,7 @@ public extension Cell where Self: UITableViewCell {
 	- returns: A dequeued table view cell object.
 	*/
 	static func dequeue(from tableView: UITableView,
-						for indexPath: IndexPath) throws -> Self {
+						at indexPath: IndexPath) throws -> Self {
 		guard
 			let dequededCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,
 															 for: indexPath) as? Self
@@ -44,6 +44,47 @@ public extension Cell where Self: UITableViewCell {
 			throw CellError.dequeueFailed
 		}
 		
+		return dequededCell
+	}
+	
+}
+
+public extension Cell where Self: UICollectionViewCell {
+	
+	// MARK: Static
+	
+	/**
+	Registers a nib object containing the cell into a collection.
+	
+	- parameter collectionView: A collection view in which to register the nib object.
+	*/
+	static func registerNib(in collectionView: UICollectionView) {
+		let bundle = Bundle(for: self)
+		let nib = UINib(nibName: nibName,
+						bundle: bundle)
+		
+		collectionView.register(nib,
+								forCellWithReuseIdentifier: reuseIdentifier)
+	}
+	
+	/**
+	Returns a reusable collection view cell object dequeued from a specified index in a collection view.
+	
+	- parameters:
+	  - collectionView: A collection view from where to dequeue the reusable cell.
+	  - indexPath: The index path specifying the location of the cell.
+	- throws: A `CellError` error is thrown during the dequeuing of the cell.
+	- returns: A dequeued collection view cell object.
+	*/
+	static func dequeue(from collectionView: UICollectionView,
+						at indexPath: IndexPath) throws -> Self {
+		guard
+			let dequededCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+																  for: indexPath) as? Self
+			else {
+				throw CellError.dequeueFailed
+		}
+
 		return dequededCell
 	}
 	
