0408f426ea5ff8e38d522285733f510cd4e224a2
Javier Cicchelli <javier@rock-n-code.com>
2018-05-25 22:45:35 +0200

    Fixed a test in UIDeviceExtensionsTests.

--- KashmirTests/iOS/UIDeviceExtensionsTests.swift
+++ KashmirTests/iOS/UIDeviceExtensionsTests.swift
@@ -25,7 +25,7 @@ class UIDeviceExtensionsTests: XCTestCase {
     // MARK: Properties tests
     
     func testVersion() {
-        XCTAssertEqual(UIDevice.current.version, 11.2)
+        XCTAssertEqual(UIDevice.current.version, 11.3)
     }
     
     func testIsPortrait() {
