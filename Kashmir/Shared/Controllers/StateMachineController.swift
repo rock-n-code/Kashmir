63578d4942395616fd2b97a08666ba19d67e7745
Javier Cicchelli <javier@rock-n-code.com>
2018-08-27 18:47:52 +0200

    Created StateMachineController.

---
+++ Kashmir/Shared/Controllers/StateMachineController.swift
@@ -0,0 +1,11 @@
+//
+//  StateMachineController.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 27/08/2018.
+//  Copyright © 2018 Rock & Code. All rights reserved.
+//
+
+public class StateMachineController {
+	
+}
---
+++ KashmirTests/Shared/Controllers/StateMachineControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  StateMachineControllerTests.swift
+//  KashmirTests
+//
+//  Created by Javier Cicchelli on 27/08/2018.
+//  Copyright © 2018 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class StateMachineControllerTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+
+}
