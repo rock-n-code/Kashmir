6c3d5328bc16eae3f9983b0d38b8167ec4c503b3
Javier Cicchelli <javier@rock-n-code.com>
2018-08-27 13:34:10 +0200

    Created FiniteState.

---
+++ Kashmir/Shared/Protocols/FiniteState.swift
@@ -0,0 +1,11 @@
+//
+//  FiniteState.swift
+//  Kashmir
+//
+//  Created by Javier Cicchelli on 27/08/2018.
+//  Copyright © 2018 Rock & Code. All rights reserved.
+//
+
+public protocol FiniteState {
+	
+}
---
+++ KashmirTests/Shared/Protocols/FiniteStateTests.swift
@@ -0,0 +1,27 @@
+//
+//  FiniteStateTests.swift
+//  KashmirTests
+//
+//  Created by Javier Cicchelli on 27/08/2018.
+//  Copyright © 2018 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+// MARK: - 
+
+class FiniteStateTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+
+}
