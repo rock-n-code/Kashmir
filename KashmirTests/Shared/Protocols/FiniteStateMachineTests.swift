276de184c83f182f1dfc559cb4868b1208d91417
Javier Cicchelli <javier@rock-n-code.com>
2018-08-27 14:11:58 +0200

    Create FiniteStateMachine.

---
+++ KashmirTests/Shared/Protocols/FiniteStateMachineTests.swift
@@ -0,0 +1,25 @@
+//
+//  FiniteStateMachineTests.swift
+//  KashmirTests
+//
+//  Created by Javier Cicchelli on 27/08/2018.
+//  Copyright © 2018 Rock & Code. All rights reserved.
+//
+
+import XCTest
+
+@testable import Kashmir
+
+class FiniteStateMachineTests: XCTestCase {
+	
+	// MARK: Setup
+	
+	override func setUp() {
+		super.setUp()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+	}
+
+}
